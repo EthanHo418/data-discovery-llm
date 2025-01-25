0b64ffd76f460ad4052d49b9bc3a9d5cf7bce6ab
Ethan Ho <discopandajedi@gmail.com>
2025-01-25 16:21:03 -0500

    Adding api key

--- tft_data_retriever.py
+++ tft_data_retriever.py
@@ -1,6 +1,20 @@
 import requests
 
 
-response = requests.get("https://developer.riotgames.com/docs/portal")
+domain_url = "https://americas.api.riotgames.com"
 
-print(response.status_code)
+summoner_name = "PPG Rex"
+
+summoner_url = f"tft/summoner/v1/summoners/by-name/{summoner_name}"
+
+params = {
+    "api_key": "RGAPI-839ecb78-0bb6-414d-9eb0-740197ebbc45"
+}
+
+response = requests.get(f"{domain_url}/{summoner_url}", params=params)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(response)
+    print(response.url)
