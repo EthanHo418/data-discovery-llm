346295e1540d29cea9f2f56ee9b1f2fc00fa85b2
carolettesaguil <csaguil0926@gmail.com>
2025-01-25 17:15:58 -0500

    get tft match history edits

--- tft_data_retriever.py
+++ tft_data_retriever.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 puuid = "8yHT2Wp4TM61Xwiw3l5vElIHVqMsiiHYyZeN5tssiye8eTtqzfpjqOGJvu-mez_4mVMKbi4b1fwGEQ"
 
@@ -10,14 +11,21 @@ summoner_url = f"tft/summoner/v1/summoners/by-name/{summoner_name}"
 
 match_url = f"tft/match/v1/matches/by-puuid/{puuid}/ids"
 
+
+
 params = {
-    "api_key": "RGAPI-839ecb78-0bb6-414d-9eb0-740197ebbc45"
+    "api_key": "RGAPI-e1c58978-31b8-4c02-b401-3e13fa388b4b"
 }
 
-response = requests.get(f"{domain_url}/{match_url}", params=params)
+match_hist_response = requests.get(f"{domain_url}/{match_url}", params=params)
+
+match_hist =  match_hist_response.json()
+match_id = match_hist[0]
+match_info_url = f"tft/match/v1/matches/{match_id}"
+response = requests.get(f"{domain_url}/{match_info_url}", params=params)
 
 if response.status_code == 200:
-    print(response.json())
+    print(json.dumps(response.json(), indent=4))
 else:
     print(response)
     print(response.url)
