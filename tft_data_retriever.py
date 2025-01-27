0bce1c39c81044b182fcf61ac93f5b8f991b65ac
Francis Ho <enkeedu@gmail.com>
2025-01-27 18:20:41 -0500

    refactor code add functions

--- tft_data_retriever.py
+++ tft_data_retriever.py
@@ -2,43 +2,63 @@ import requests
 import json
 import os
 
-api_key = "RGAPI-57179589-e02c-4645-b464-fdfad4485114"
+API_KEY = "RGAPI-57179589-e02c-4645-b464-fdfad4485114"
+DOMAIN_URL = "https://americas.api.riotgames.com"
+ACCOUNT_URL = "riot/account/v1/accounts/by-riot-id/{gamer_name}/{tag_line}"
+MATCH_URL = "tft/match/v1/matches/by-puuid/{puuid}/ids"
+MATCH_INFO_URL = "tft/match/v1/matches/{match_id}"
+
 
 def get_stored_match_ids():
     directory_path = 'data/match/'
     files = os.listdir(directory_path)
     files = [f for f in files if os.path.isfile(os.path.join(directory_path, f))]
-    files =[f.replace('.json', '') for f in files]
+    files = [f.replace('.json', '') for f in files]
     return files
 
 
+def get_account_info(gamer_name, tag_line):
+    params = {
+        'api_key': API_KEY,
+    }
+    full_url = f"{DOMAIN_URL}/{ACCOUNT_URL}"
+    full_url = full_url.format(gamer_name=gamer_name, tag_line=tag_line)
+    response = requests.get(full_url, params=params)
+    return response.json()
 
-retrieved_matches = get_stored_match_ids()
 
-tag_line = 'PPG'
-summoner_name = "PPG Rex"
+def get_match_ids(puuid):
+    params = {
+        'api_key': API_KEY,
+    }
+    url = f"{DOMAIN_URL}/{MATCH_URL}"
+    url = url.format(puuid=puuid)
+    response = requests.get(url, params=params)
+    return response.json()
 
-puuid = "DqNKF8vPZ9JKsUhKOQ39ijH2p3w660wHowxqnBPnihBgeHyj4Ws7LS9xlHm2lY9claiP_ztgZcDjDQ"
-domain_url = "https://americas.api.riotgames.com"
-summoner_url = f"tft/summoner/v1/summoners/by-name/{summoner_name}"
-match_url = f"tft/match/v1/matches/by-puuid/{puuid}/ids"
-params = {
-    "api_key": api_key
-}
 
-match_hist_response = requests.get(f"{domain_url}/{match_url}", params=params)
+def get_match_info(match_id):
+    params = {
+        'api_key': API_KEY,
+    }
+    url = f"{DOMAIN_URL}/{MATCH_INFO_URL}"
+    url = url.format(match_id=match_id)
+    response = requests.get(url, params=params)
+    return response.json()
 
-match_hist =  match_hist_response.json()
-for match_id in match_hist:
-    if match_id in retrieved_matches:
-        continue
-    match_info_url = f"tft/match/v1/matches/{match_id}"
-    response = requests.get(f"{domain_url}/{match_info_url}", params=params)
 
-    if response.status_code == 200:
+def persist_matches(gamer_name, tag_line):
+    account_info = get_account_info(gamer_name, tag_line)
+    match_ids = get_match_ids(account_info['puuid'])
+    stored_match_ids = get_stored_match_ids()
+    for match_id in match_ids:
+        if match_id in stored_match_ids:
+            continue
+        match_info = get_match_info(match_id)
         with open(f'data/match/{match_id}.json', 'w') as f:
-            f.write(json.dumps(response.json(), indent=4))
-    else:
-        print(response)
-        print(response.url)
-        print("https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/8yHT2Wp4TM61Xwiw3l5vElIHVqMsiiHYyZeN5tssiye8eTtqzfpjqOGJvu-mez_4mVMKbi4b1fwGEQ/ids?start=0&count=20&api_key=RGAPI-e1c58978-31b8-4c02-b401-3e13fa388b4b")
+            f.write(json.dumps(match_info, indent=4))
+
+
+if __name__ == '__main__':
+    persist_matches('PPG Rex', 'PPG')
+    persist_matches('StuckStepLaner', 'NA1')
