c492db713b4dadc0c868a023b6016842000f66f1
Francis Ho <enkeedu@gmail.com>
2025-01-27 19:35:05 -0500

    add parsing logics for matches nad participant tables

--- tft_data_retriever.py
+++ tft_data_retriever.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import os
+import csv
+from datetime import datetime
 
 API_KEY = "RGAPI-57179589-e02c-4645-b464-fdfad4485114"
 DOMAIN_URL = "https://americas.api.riotgames.com"
@@ -74,6 +76,52 @@ def persist_matches(gamer_name, tag_line):
             f.write(json.dumps(match_info, indent=4))
 
 
+def parse_match_info(file_path):
+    with open(file_path, 'r') as f:
+        match_info = json.load(f)
+    return match_info
+
+
+def persist_csv(file_path, data):
+    with open(file_path, mode="w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+
+
 if __name__ == '__main__':
-    persist_matches('PPG Rex', 'PPG')
-    persist_matches('StuckStepLaner', 'NA1')
+    # persist_matches('PPG Rex', 'PPG')
+    # persist_matches('StuckStepLaner', 'NA1')
+    match_info = parse_match_info('data/match/NA1_5193017032.json')
+    match_data = []
+
+    game_datetime = str(datetime.fromtimestamp(match_info['info']['game_datetime'] / 1000))
+    match_data.append(
+        {
+            "match_id": match_info['metadata']['match_id'],
+            "game_datetime": game_datetime,
+            "game_length": match_info['info']['game_length'],
+            "game_version": match_info['info']['game_version'],
+            "queue_id": match_info['info']['queue_id'],
+            "end_game_result": match_info['info']['endOfGameResult'],
+            "game_id": match_info['info']['gameId'],
+            "map_id": match_info['info']['mapId'],
+        }
+    )
+    persist_csv('matches.csv', match_data)
+
+    participant_data = []
+    for participant in match_info['info']['participants']:
+        participant_data.append(
+            {
+                "match_id": match_info['metadata']['match_id'],
+                "puuid": participant['puuid'],
+                "gamer_name": participant['riotIdGameName'],
+                "tag_line": participant['riotIdTagline'],
+                "partner_group_id": participant['partner_group_id'],
+                "companion": participant['companion']['species'],
+                "gold_left": participant['gold_left'],
+                "placement": participant['placement']
+            }
+        )
+    persist_csv('participants.csv', participant_data)
